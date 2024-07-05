c29e35ae26f0186069da27eadc627ea19ef6a635
Samuel A <samakinbile@gmail.com>
2024-07-05 12:59:24 +0000

    coffee

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coffee',
+    
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-samakinbile-coffeeshop-0p8z40g5hzj.ws-eu115.gitpod.io']
