af56bae34660d06fd9152273b6ecae98c2090ce6
Samuel Akinbile <samakinbile@gmail.com>
2024-07-08 17:46:28 +0000

    hard rest

--- coffee/urls.py
+++ coffee/urls.py
@@ -1,6 +1,10 @@
+# urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('add_to_cart/<int:coffee_id>/', views.add_to_cart, name='add_to_cart')
 ]
--- coffee/views.py
+++ coffee/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Coffee
 
 def home(request):
     coffee = Coffee.objects.all()
     return render(request, 'home.html', {'coffee': coffee})
+
+def checkout(request):
+    coffee = Coffee.objects.all()
+    return render(request, 'checkout.html', {'coffee': coffee})
