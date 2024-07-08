c8fa1ccd2a7af0f0b6d216220e082f3932f3a5f5
Samuel Akinbile <samakinbile@gmail.com>
2024-07-08 18:10:03 +0000

    hard rest

--- coffee/urls.py
+++ coffee/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('checkout/', views.checkout, name='checkout'),
-    path('add_to_cart/<int:coffee_id>/', views.add_to_cart, name='add_to_cart')
+     
 ]
