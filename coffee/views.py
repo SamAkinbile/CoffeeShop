f1515a69605ee3cd159f9838dce40d1c1d19b3ff
Samuel Akinbile <samakinbile@gmail.com>
2024-07-10 19:08:44 +0000

    no error

--- coffee/views.py
+++ coffee/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+# views.py
+
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Coffee
 
 def home(request):
@@ -6,5 +8,14 @@ def home(request):
     return render(request, 'home.html', {'coffee': coffee})
 
 def checkout(request):
-    coffee = Coffee.objects.all()
-    return render(request, 'checkout.html', {'coffee': coffee})
+    cart_items = CartItem.objects.all()
+    total = sum(item.coffee.price * item.quantity for item in cart_items)
+    return render(request, 'checkout.html', {'cart_items': cart_items, 'total': total})
+
+def add_to_cart(request, coffee_id):
+    coffee = get_object_or_404(Coffee, id=coffee_id)
+    cart_item, created = CartItem.objects.get_or_create(coffee=coffee)
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()
+    return redirect('home')
